7f91d1b26f7bd67d952132bdd19538dc1cf2c69b
SuanneBarbosa <suanne.a.barbosa@gmail.com>
2022-06-13 22:15:03 -0300

    versão inicial do jogo

--- projeto.c
+++ projeto.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct
 {
@@ -24,6 +25,7 @@ LEVEL nextLevel;    // proximo nivel
 
 int currentPuzzleRandomNumber;
 char currentPuzzleResponse[200];
+ int errorCount = 0;
 
 // função de carregar o nível
 
@@ -129,7 +131,11 @@ int main() // dentro da main so manipula os dados
     // Configurações iniciais
     
 
-    printf("Você entra numa casa abandonada na espera de encontrar algum tesouro antigo (ou pelo menos algo que possa vender por um preço razoável). Você lembra do rosto do velho enquanto era levado pelas ambulâncias: tão sem vida quanto a casa que ele morou nos últimos 2 meses.\nEle havia se mudado pra vizinhança após ter sua casa no centro da cidade tomada por dívidas, então você não espera encontrar muita coisa dele, mas a casa esteve de pé por muitos anos antes e a família Skalo que sempre a habitou não parecia ser tão 'pé-rapada' quanto o velho senhor que provavelmente a conseguiu por uma pechincha.\nO motivo pela mudança de senhorio, ainda que peculiar, foi o que te trouxe aqui neste momento. No último ano, cada um dos 6 membros da família Skalo faleceu de formas estranhas (para não dizer suspeitas).\nA forma súbita que as mortes se sucederam te fizeram imaginar que seus bens permaneceram na casa, já que eram os últimos integrantes da família antiga, e roubar uma casa abandonada sem ninguém pra dar falta é muito melhor que invadir as residências dos rostos conhecidos na região, sempre com o risco de ser pego ou reconhecido. Hoje, ao ver as ambulâncias saindo, você decidiu tentar a sorte.\n Agora, você tem as seguintes opções:\n");
+    printf("Você entra numa casa abandonada na espera de encontrar algum tesouro antigo (ou pelo menos algo que possa vender por um preço razoável). Você lembra do rosto do velho enquanto era levado pelas ambulâncias: tão sem vida quanto a casa que ele morou nos últimos 2 meses.\nEle havia se mudado pra vizinhança após ter sua casa no centro da cidade tomada por dívidas, então você não espera encontrar muita coisa dele, mas a casa esteve de pé por muitos anos antes e a família Skalo que sempre a habitou não parecia ser tão 'pé-rapada' quanto o velho senhor que provavelmente a conseguiu por uma pechincha.\nO motivo pela mudança de senhorio, ainda que peculiar, foi o que te trouxe aqui neste momento. No último ano, cada um dos 6 membros da família Skalo faleceu de formas estranhas (para não dizer suspeitas).\nA forma súbita que as mortes se sucederam te fizeram imaginar que seus bens permaneceram na casa, já que eram os últimos integrantes da família antiga, e roubar uma casa abandonada sem ninguém pra dar falta é muito melhor que invadir as residências dos rostos conhecidos na região, sempre com o risco de ser pego ou reconhecido. Hoje, ao ver as ambulâncias saindo, você decidiu tentar a sorte.\n");
+
+    // printf("Digite qualquer tecla para continuar");
+    // scanf("%s", &userAnswer);
+    // fflush (stdin);
 
     currentLevel = levelList[0]; // passo inicial
 
@@ -178,12 +184,78 @@ int main() // dentro da main so manipula os dados
             }           
          }
          else {
-            // puzzle
+            printf("Desvende o enigma ou escolha uma das seguintes opções:\najuda\nvoltar\nsair\n");
+            
+            //criar o swith case
+            //gerar um numero aleatorio de 0 a quatidade de enigmas possiveis
+            // gerar uma pergunta e uma resposta dependendo do caso
+            //para mesma logica a resposta gerada fica no lugar do correct do nivel do puzzle e depois percorrer as respostas ou so salva em uma variavel e compara
+            // depois fazer o mesmo if else do tipo normal e da pra fazer em uma função copia do ajuda e do sair e voltar 
+            int puzzle = rand() % 3;
+            char puzzleAnswer[200];
+           
+
+            switch (puzzle)
+            {
+            case 0 : 
+                printf("enigma 0\n");
+                strcpy(puzzleAnswer, "resposta0");
+                break;
+            case 1 : 
+                printf("enigma 1\n");
+                strcpy(puzzleAnswer, "resposta1");
+                break;
+            case 2 : 
+                printf("enigma 2\n");
+                strcpy(puzzleAnswer, "resposta2");
+                break;    
+            default:
+                printf("enigma 3\n");
+                strcpy(puzzleAnswer, "resposta3");
+                break;
+            }
+            scanf("%s", &userAnswer);
+            printf("Sua resposta foi %s!\n", userAnswer);
+                
+                // opções extras, ajuda e sair 
+        
+            if (strcmp(userAnswer , "ajuda") == 0)
+            {
+                printf("Vou te ajudar!\n");
+            }
+            else if (strcmp(userAnswer , "sair") == 0)
+            {
+                printf("Bye Bye!\n");
+                return 0;
+            }
+            else if (strcmp(userAnswer , "voltar") == 0)
+            {
+                currentLevel = levelList[currentLevel.answerList[1].path];
+            }
+            else if (strcmp(userAnswer , puzzleAnswer) == 0)
+            {
+                currentLevel = levelList[currentLevel.answerList[0].path];
+            }
+            
+            else { // comparar as respostas e níveis
+                errorCount++;
+                if (errorCount == 3)
+                {
+                   printf("Você perdeu o jogo!");
+                   return 0;
+                }
+                else {
+                    printf("Você errou e tem mais %d tentativas\n", 3 - errorCount);
+                }
+                
+            }           
          }
          
         
         
     }
+
+    printf("Parabéns você ganhou o jogo!");
 
     return 0;
 }
